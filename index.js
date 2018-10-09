430ba1748b63e58f80733f59a9502fcf145e2d27
Dustin Coffey <dustin@cass-solutions.com>
2018-10-09 14:52:04 -0400

    Remove use of underscore. Clean up flow and scoping.

--- index.js
+++ index.js
@@ -6,9 +6,9 @@
 // git@github.com:Moveline/mongoose-eventify.git
 
 // Mongoose plugin adding add, remove, and change events for attributes on Model class
-var _ = require('underscore');
 
-module.exports = exports = function events(schema, options) {
+module.exports = function events(schema, options) {
+
   schema.pre('save', function (next) {
     this._changed = this.modifiedPaths();
     this._isNew = this.isNew;
@@ -16,29 +16,35 @@ module.exports = exports = function events(schema, options) {
   });
 
   schema.post('save', function() {
-    var model = this.model(this.constructor.modelName);
-    if(this._changed) {
-      model.emit('change', this);
+    const model = this.model(this.constructor.modelName);
 
-      var self = this;
-      var eventKeys = {};
-      _.each(this._changed, function(attribute) {
-        var key = 'change:' + attribute;
-        if (typeof eventKeys[key] === 'undefined') {
-          model.emit(key, self);
-          eventKeys[key] = key;
-        }
-      });
-      delete this._changed;
-    }
     if (this._isNew) {
       model.emit('add', this);
       delete this._isNew;
+      return;
     }
+
+    if (this._changed.length) {
+
+      model.emit('change', this);
+
+      const emitted_keys = [];
+
+      this._changed.forEach(path => {
+        const key = 'change:' + path;
+        if (emitted_keys.includes(key)) return;
+        model.emit(key, this);
+        emitted_keys.push(key);
+      });
+
+      delete this._changed;
+    }
+
   });
 
   schema.post('remove', function() {
-    var model = this.model(this.constructor.modelName);
+    const model = this.model(this.constructor.modelName);
     model.emit('remove', this);
   });
+
 }
